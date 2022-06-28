a27010e15af75bbaaa295934cfbf2bb48f759b9c
Zxython <98050815+Zxython@users.noreply.github.com>
2022-06-28 16:39:56 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='statpy',
+      version='0.0.1',
+      description='Adds statistical stuff',
+      url='https://github.com/Zxython/statpy/',
+      author='Jerry Gillian',
+      author_email='jerrygillian822@gmail.com',
+      license='Free to use',
+      packages=['statpy'],
+      zip_safe=False)
